79792dd8d850e1fce5f5848972d06583a40c28d9
Dominick <Domdominick@hotmail.com>
2020-01-04 09:08:44 -0500

    card

---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css"
+
+function Card({ clickHandler, id, image, name }) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img
+                    src={image}
+                    alt={name}
+                    onClick={() => clickHandler(id)}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Card;
